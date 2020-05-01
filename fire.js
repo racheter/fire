94be4ba1de0a9a6d1156bcc0f2a8da10c46d8727
racheter <44201656+racheter@users.noreply.github.com>
2020-04-30 21:29:28 -0300

    Add files via upload

--- fire.js
+++ fire.js
@@ -1,14 +1,14 @@
 const fP = []
-const fW = 60
-const fH = 60
-const color = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
+const fW = 85
+const fH = 85
+const color = [{ "r": 7, "g": 7, "b": 7 }, { "r": 5, "g": 7, "b": 7 }, { "r": 5, "g": 15, "b": 7 }, { "r": 5, "g": 15, "b": 7 }, { "r": 5, "g": 23, "b": 7 }, { "r": 10, "g": 30, "b": 7 }, { "r": 5, "g": 50, "b": 7 }, { "r": 5, "g": 60, "b": 7 }, { "r": 5, "g": 70, "b": 7 }, { "r": 5, "g": 75, "b": 7 }, { "r": 5, "g": 80, "b": 7 }, { "r": 10, "g": 90, "b": 7 }, { "r": 5, "g": 100, "b": 7 }, { "r": 5, "g": 110, "b": 7 }, { "r": 10, "g": 115, "b": 7 }, { "r": 10, "g": 120, "b": 7 }, { "r": 30, "g": 125, "b": 7 }, { "r": 10, "g": 140, "b": 15 }, { "r": 20, "g": 150, "b": 15 }, { "r": 5, "g": 165, "b": 15 }, { "r": 10, "g": 175, "b": 15 }, { "r": 10, "g": 180, "b": 23 }, { "r": 10, "g": 190, "b": 23 }, { "r": 10, "g": 205, "b": 23 }, { "r": 10, "g": 205, "b": 21 }, { "r": 12, "g": 205, "b": 21 }, { "r": 15, "g": 210, "b": 21 }, { "r": 20, "g": 210, "b": 39 }, { "r": 15, "g": 220, "b": 20 }, { "r": 20, "g": 230, "b": 20 }, { "r": 75, "g": 230, "b": 55 }, { "r": 74, "g": 240, "b": 55 }, { "r": 67, "g": 240, "b": 38 }, { "r": 67, "g": 245, "b": 38 }, { "r": 125, "g": 245, "b": 45 }, { "r": 171, "g": 250, "b": 118 }, { "r": 255, "g": 255, "b": 255 }]
 
 function iniciar() {
     estrutura()
     criarf()
     fogo()
 
-    setInterval(calculo, 50)
+    setInterval(calculo, 20)
 }
 
 function estrutura() {
@@ -38,7 +38,7 @@ function acalculo(movivalor) {
         return
     }
 
-    const desconto = Math.floor(Math.random() * 3)
+    const desconto = Math.floor(Math.random() * 2)
     const intencidade = fP[analise]
     const novai =
         intencidade - desconto >= 0 ? intencidade - desconto : 0
@@ -57,20 +57,10 @@ function fogo() {
         for (let coluna = 0; coluna < fW; coluna++) {
             const valor = coluna + (fW * linha)
             const fvalor = fP[valor]
-
-            if (debug === true) {
-                html += '<td>'
-                html += '<div class="pixel-valor">'
-                html += valor
-                html += '</div>'
-                html += fvalor
-                html += '</td>'
-            } else {
-                const colorf = color[fvalor]
-                const colores = `${colorf.r},${colorf.g},${colorf.b}`
-                html += `<td class="pixel" style="background-color: rgb(${colores})">`
-                html += '</td>'
-            }
+            const colorf = color[fvalor]
+            const colores = `${colorf.r},${colorf.g},${colorf.b}`
+            html += `<td class="pixel" style="background-color: rgb(${colores})">`
+            html += '</td>'
         }
 
         html += '</tr>'
